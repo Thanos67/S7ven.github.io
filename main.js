79706a538316abd33a10cd64ef686ce69065bd81
Thanos67 <thanasakis199867@gmail.com>
2020-11-21 16:18:57 +0200

    Add map

--- main.js
+++ main.js
@@ -1,3 +1,21 @@
 $(document).ready(function(){
   $('.header').height($(window).height());
 })
+
+// Initialize and add the map
+function initMap() {
+  // The location of Uluru
+  const uluru = { lat: 37.0413133627007, lng: 22.10328859047705 };
+  // The map, centered at Uluru
+  const map = new google.maps.Map(document.getElementById("map"), {
+    zoom: 13,
+    center: uluru,
+	
+  });
+  // The marker, positioned at Uluru
+  const marker = new google.maps.Marker({
+    position: uluru,
+    map: map,
+  });
+}
+
